ff264e1679ee8d47eef5d733c6bab0569be50158
lrodenyoder <rodenlau000@gmail.com>
2022-01-07 17:21:18 -0600

    beginning pass length js

--- Develop/script.js
+++ Develop/script.js
@@ -6,10 +6,30 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  //BEGIN PASSWORD LENGTH CODE
+  var passLength = window.prompt("Select length of password between 8 - 128 characters.");
+
+  if (isNaN(passLength) || passLength === "" || passLength === null) {
+    window.alert("Please enter a valid number between 8 - 128.");
+
+    return writePassword();
+  }
+
+  passLength = parseInt(passLength);
+
+  if (passLength < 8 || 128 < passLength) {
+    window.alert("Please try again. Select length of password between 8-128 characters.");
+
+    return writePassword();
+  }
+  //END PASSWORD LENGTH Code
+
+  
+
+  // passwordText.value = password;
 
 }
 
